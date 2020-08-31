27211c39210826afb2f7b94a925c6189251159a5
hogoww <pierre_misse25@msn.com>
2020-08-31 21:01:00 +0200

    add short script to put everything in the directory for compilation tests

---
+++ movestuff.sh
@@ -0,0 +1,4 @@
+cp ~/Desktop/main.c ~/ovm/build/vm
+cp ~/Pharo/images/Fangleureu/generated/classes/*.h ~/ovm/build/vm
+cp ~/ovm/generated/vm/src/*.h ~/ovm/build/vm
+cp ~/ovm/generated/vm/includes/*.h ~/ovm/build/vm
