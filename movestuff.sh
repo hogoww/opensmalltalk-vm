12fefd129d98c908d879f7721cf012d0c4b38c13
hogoww <pierre_misse25@msn.com>
2020-09-13 08:13:46 +0200

    fix an error in the script

--- movestuff.sh
+++ movestuff.sh
@@ -1,4 +1,4 @@
-cp ~/Desktop/main.c ~/ovm/build/vm
+cp ~/ovm/main.c ~/ovm/build/vm
 cp ~/Pharo/images/Fangleureu/generated/classes/*.h ~/ovm/build/vm
-cp ~/ovm/generated/vm/src/*.h ~/ovm/build/vm
-cp ~/ovm/generated/vm/includes/*.h ~/ovm/build/vm
+cp ~/ovm/generated/vm/src/* ~/ovm/build/vm
+cp ~/ovm/generated/vm/include/*.h ~/ovm/build/vm
