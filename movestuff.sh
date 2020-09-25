52962bd17d7f0d92015d9eaec86e86a642b185e3
hogoww <pierre_misse25@msn.com>
2020-09-25 11:46:04 +0200

    removeFile

--- movestuff.sh
+++
@@ -1,4 +0,0 @@
-cp ~/Desktop/main.c ~/ovm/build/vm
-cp ~/Pharo/images/Fangleureu/generated/classes/*.h ~/ovm/build/vm
-cp ~/ovm/generated/vm/src/*.h ~/ovm/build/vm
-cp ~/ovm/generated/vm/includes/*.h ~/ovm/build/vm
