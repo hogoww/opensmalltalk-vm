7c47cec0d8c2f939be323aba6727d326ce11c83f
hogoww <pierre_misse25@msn.com>
2020-10-02 15:03:50 +0200

    [improvement][cleanup] started to cleanup and now compiles to a new directory

--- movestuff.sh
+++ movestuff.sh
@@ -1,8 +1,12 @@
-cp ./main.c ./build/vm
-cp ./setupImage.c ./build/vm
-cp ~/Pharo/images/Fangleureu/generated/classes/*.h ./build/vm
-cp ~/Pharo/images/Fangleureu/generated/tests/*.h ./build/vm
-cp ./generated/vm/src/* ./build/vm
-cp ./generated/vm/include/*.h ./build/vm
-cp ./tempconversion-64.image ./build/vm
-cp ./CuTest.* ./build/vm
+echo "moving stuff into directory $1"
+
+
+cp ./builder/vm/* $1
+cp ./main.c $1
+cp ./setupImage.c $1
+cp ~/Pharo/images/Fangleureu/generated/classes/*.h $1
+cp ~/Pharo/images/Fangleureu/generated/tests/*.h $1
+cp ./generated/vm/src/* $1
+cp ./generated/vm/include/*.h $1
+cp ./tempconversion-64.image $1
+cp ./CuTest.* $1
