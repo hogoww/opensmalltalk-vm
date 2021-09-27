888ff79ee9cebe7e507cdcd52f1cce61bf02fab4
hogoww <pierre_misse25@msn.com>
2021-09-27 11:22:55 +0200

    add move of llDictionary

--- movestuff.sh
+++ movestuff.sh
@@ -22,6 +22,8 @@ cp ./generated/vm/src/* $1
 cp ./generated/vm/include/*.h $1
 cp ./tempConversion-64.image $1
 cp ./src/loadImage.c $1
+cp ../llstructures/llDictionary.c $1
+cp ../llstructures/llDictionary.h $1
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     cp ~/Pharo/images/muOvm/generated/tests/*.h $1
