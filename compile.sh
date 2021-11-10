b63ee63d3f4102e5e265b379eff9e354c7eed2cb
hogoww <pierre_misse25@msn.com>
2021-11-10 16:13:28 +0100

    update scripts

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ targetDirectory="testCompilation"
 rm -rf CMakeCache.txt CMakeFiles/ build/ generated/ Illicium/ $targetDirectory
 mkdir build
 cmake -S ./ -B build -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=True
-cd build && make
+make -C build
 
 mkdir $targetDirectory
 
--- movestuff.sh
+++ movestuff.sh
@@ -7,7 +7,7 @@ echo "moving stuff into directory $1"
 vmLibraryDirectory=""
 #imagePath=""
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    vmLibraryDirectory="./build/vm/*"
+    vmLibraryDirectory="./build/build/vm/*"
     #imagePath="~/Pharo/images/"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     vmLibraryDirectory="./build/vm/Pharo.app/Contents/MacOS/Plugins/*"
@@ -15,22 +15,19 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 cp $vmLibraryDirectory $1
-cp ./main.c $1
-cp ./setupImage.c $1
-#cp -v $imagePath$imageName/generated/tests/*.h $1
-cp ./generated/vm/src/* $1
-cp ./generated/vm/include/*.h $1
+cp ./build/generated/64/vm/src/* $1
+cp ./build/generated/64/vm/include/*.h $1
 cp ./tempConversion-64.image $1
 cp ./src/loadImage.c $1
-cp ./generatedV
+cp ../llstructures/llDictionary.* $1
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    cp ~/Pharo/images/muovm/generated/tests/Makefile $1
-    cp ~/Pharo/images/muovm/generated/tests*.makefile $1
-    cp ~/Pharo/images/muovm/generated/tests/*.h $1
-    cp ~/Pharo/images/muovm/generated/tests/*.c $1
+    cp ~/Pharo/images/muOvm/generated/tests/Makefile $1
+    cp ~/Pharo/images/muOvm/generated/tests/*.makefile $1
+    cp ~/Pharo/images/muOvm/generated/tests/*.h $1
+    cp ~/Pharo/images/muOvm/generated/tests/*.c $1
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    cp ~/Documents/Pharo/images/muov2m/generated/tests/Makefile $1
+    cp ~/Documents/Pharo/images/muovm2/generated/tests/Makefile $1
     cp ~/Documents/Pharo/images/muovm2/generated/tests/*.makefile $1
     cp ~/Documents/Pharo/images/muovm2/generated/tests/*.h $1
     cp ~/Documents/Pharo/images/muovm2/generated/tests/*.c $1
