174552a230f0f50ea11d7bff38921b3432a168dc
hogoww <pierre_misse25@msn.com>
2021-12-05 12:58:00 +0100

    update moveStuff

--- compile.sh
+++ compile.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-targetDirectory="testCompilation"
 
-
-rm -rf CMakeCache.txt CMakeFiles/ build/ generated/ Illicium/ $targetDirectory
+rm -rf CMakeCache.txt CMakeFiles/ build/ generated/ Illicium/
 mkdir build
 cmake -S ./ -B build -DPHARO_DEPENDENCIES_PREFER_DOWNLOAD_BINARIES=True
 make -C build
 
-mkdir $targetDirectory
-
-./movestuff.sh $targetDirectory
+./movestuff.sh
 
 
