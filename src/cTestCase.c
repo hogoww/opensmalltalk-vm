45551c1919c38eb648d2a045ae684084752d3ebe
hogoww <pierre_misse25@msn.com>
2021-12-05 22:46:05 +0100

    add back cTestCase.c move

---
+++ src/cTestCase.c
@@ -0,0 +1,1 @@
+/* preamble */#include <stdio.h>#include <stdlib.h>void testPassed(){  printf("Test passed\n");  exit(EXIT_SUCCESS);}void testFailed(int line){  printf("Assertion line: %d Test failed\n", line);  exit(EXIT_FAILURE);}void cAssert(int line, int condition){  if (condition) return;  testFailed(line);}
