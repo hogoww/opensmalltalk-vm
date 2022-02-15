6c9603b67eab8fb401304c49085b5c8bf5880b68
hogoww <pierre_misse25@msn.com>
2022-02-15 15:29:20 +0100

    define dependency differently

--- cmake/vmmaker.cmake
+++ cmake/vmmaker.cmake
@@ -17,6 +17,8 @@
 
 set(CMAKE_VERBOSE_MAKEFILE TRUE)
 
+add_subdirectory(smalltalksrc ${CMAKE_CURRENT_BINARY_DIR}/smalltalksrc)
+
 #Setting vmmaker directory and image 
 set( VMMAKER_DIR    "${CMAKE_CURRENT_BINARY_DIR_TO_OUT}/build/vmmaker")
 set( VMMAKER_IMAGE  "${VMMAKER_DIR}/image/VMMaker.image")
@@ -131,7 +133,7 @@ if(GENERATE_SOURCES)
     add_custom_command(
         OUTPUT ${VMSOURCEFILES} ${PLUGIN_GENERATED_FILES}
         COMMAND ${VMMAKER_VM} --headless ${VMMAKER_IMAGE} --no-default-preferences eval \"PharoVMMaker generate: \#\'${FLAVOUR}\' outputDirectory: \'${CMAKE_CURRENT_BINARY_DIR_TO_OUT}\'\"
-        DEPENDS build_vmmaker_get_image ${CMAKE_CURRENT_SOURCE_DIR_TO_OUT}/smalltalksrc
+        DEPENDS build_vmmaker_get_image smalltalksrc
         COMMENT "Generating VM files for flavour: ${FLAVOUR}")
     
     add_custom_target(vmmaker DEPENDS build_vmmaker_get_image)
